e916155228af4cfdd7c18a987f342e5cf1d49760
Elana Ades <elanaades1@gmail.com>
2023-05-07 15:45:48 -0400

    Add project files.

---
+++ ReactPeopleBasic.Web/ClientApp/src/PeopleTable.jsx
@@ -0,0 +1,87 @@
+﻿import React from 'react';
+import PersonForm from './PersonForm';
+import PersonRow from './PersonRow';
+//import Person from './Person';
+
+class PeopleTable extends React.Component {
+
+
+    state = {
+        currentFirstName: '',
+        currentLastName: '',
+        currentAge: '',
+        //color: '',
+        people: []
+    }
+
+    onFirstNameTextChange = (e) => {
+        this.setState({ currentFirstName: e.target.value });
+    }
+
+    onLastNameTextChange = (e) => {
+        this.setState({ currentLastName: e.target.value });
+    }
+
+    onAgeTextChange = (e) => {
+        this.setState({ currentAge: e.target.value });
+    }
+
+    onClearClick = () => {
+        this.setState({ currentFirstName: '' });
+        this.setState({ currentLastName: '' });
+        this.setState({ currentAge: '' });
+    }
+
+    onAddClick = () => {
+        const copy = [...this.state.people];
+        const person = {
+            firstName: this.state.currentFirstName,
+            lastName: this.state.currentLastName,
+            age: this.state.currentAge,
+            //color: this.getRandomColor()
+        }
+        copy.push(person);
+
+        this.setState({ people: copy, currentFirstName: '', currentLastName: '', currentAge: '' });
+    }
+
+    //getRandomColor() {
+    //    const colors = ['linen', 'aliceblue', 'lavender', 'lilac', 'lemonchiffon', 'mint', 'thistle'];
+    //    return colors[Math.floor(Math.random() * colors.length)];
+    //}
+
+
+    render() {
+        return (
+            <div className="container mt-5">
+
+                <PersonForm currentFirstName={this.state.currentFirstName}
+                    currentLastName={this.state.currentLastName}
+                    currentAge={this.state.currentAge}
+                    onFirstNameTextChange={this.onFirstNameTextChange}
+                    onLastNameTextChange={this.onLastNameTextChange}
+                    onAgeTextChange={this.onAgeTextChange}
+                    onClearClick={this.onClearClick}
+                    onAddClick={this.onAddClick}
+                />
+
+                {this.state.people.length === 0 ?
+                    <h2 style={{ marginTop: "25px" }}>No people added yet! Start adding...</h2> :
+                    <table className='table table-hover table-bordered' style={{ marginTop: "25px" }}>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <PersonRow people={this.state.people} />
+                        </tbody>
+                    </table>
+                }
+            </div>);
+    };
+};
+
+export default PeopleTable;
---
+++ ReactPeopleBasic.Web/ClientApp/src/PersonForm.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+
+class PersonForm extends React.Component {
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-md-3'>
+                    <input value={this.props.currentFirstName} onChange={this.props.onFirstNameTextChange} type='text' placeholder='First Name' className='form-control' />
+                </div>
+
+                <div className='col-md-3'>
+                    <input value={this.props.currentLastName} onChange={this.props.onLastNameTextChange} type='text' placeholder='Last Name' className='form-control' />
+                </div>
+
+                <div className='col-md-3'>
+                    <input value={this.props.currentAge} onChange={this.props.onAgeTextChange} type='text' placeholder='Age' className='form-control' />
+                </div>
+
+                <div className='col-md-1'>
+                    <button onClick={this.props.onAddClick} className='btn btn-outline-primary w-100'>Add</button>
+                </div>
+                <div className='col-md-1'>
+                    <button onClick={this.props.onClearClick} className='btn btn-outline-danger w-100'>Clear</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PersonForm;
